b53083e646f73b4fc19203be47407e15bcaa43b7
Sanu Khan-Business Partner <sanu.khan-external@alfuttaim.com>
2024-07-16 13:06:43 +0400

    Next js webpack config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const path = require('path');
+
+const nextConfig ={
   reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@/components': path.resolve(__dirname, 'src/components'),
+      '@/styles': path.resolve(__dirname, 'src/styles'),
+      '@/images': path.resolve(__dirname, 'src/assets/images'),
+      '@/vendors': path.resolve(__dirname, 'src/assets/vendors'),
+      '@/context': path.resolve(__dirname, 'src/context'),
+      '@/hooks': path.resolve(__dirname, 'src/hooks'),
+      '@/data': path.resolve(__dirname, 'src/data'),
+    };
+    return config;
+  },
 }
 
 module.exports = nextConfig
