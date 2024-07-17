c224db9b1b85673d5ac4e3ef0911b17b832d1e21
Sanu Khan-Business Partner <sanu.khan-external@alfuttaim.com>
2024-07-17 12:22:09 +0400

    home page chage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,33 @@
-import AboutOne from "../components/AboutOne/AboutOne";
-import BrandOne from "../components/BrandOne/BrandOne";
-import DestinationsOne from "../components/DestinationsOne/DestinationsOne";
-import GalleryOne from "../components/GalleryOne/GalleryOne";
-import Layout from "../components/Layout/Layout";
-import MainSlider from "../components/MainSlider/MainSlider";
-import NewsOne from "../components/NewsOne/NewsOne";
-import PopularTours from "../components/PopularTours/PopularTours";
-import TestimonialOne from "../components/TestimonialOne/TestimonialOne";
-import TourSearch from "../components/TourSearch/TourSearch";
-import VideoOne from "../components/VideoOne/VideoOne";
-import WhyChoose from "../components/WhyChoose/WhyChoose";
+import AboutTwo from "@/components/AboutTwo/AboutTwo";
+import BannerTwo from "@/components/BannerTwo/BannerTwo";
+import BookNow from "@/components/BookNow/BookNow";
+import BrandTwo from "@/components/BrandTwo/BrandTwo";
+import CounterOne from "@/components/CounterOne/CounterOne";
+import DestinationsTwo from "@/components/DestinationsTwo/DestinationsTwo";
+import Layout from "@/components/Layout/Layout";
+import NewsTwo from "@/components/NewsTwo/NewsTwo";
+import PopularToursTwo from "@/components/PopularTours/PopularToursTwo";
+import SingaporeTour from "@/components/SingaporeTour/SingaporeTour";
+import ThreeIconBox from "@/components/ThreeIconBox/ThreeIconBox";
+import TourTypes from "@/components/TourTypes/TourTypes";
+import VideoTwo from "@/components/VideoTwo/VideoTwo";
 import React from "react";
 
 const Home = () => {
   return (
-    <Layout pageTitle="Home One">
-      <MainSlider />
-      <TourSearch />
-      <DestinationsOne />
-      <AboutOne />
-      <PopularTours />
-      <VideoOne />
-      <BrandOne />
-      <TestimonialOne />
-      <GalleryOne />
-      <WhyChoose />
-      <NewsOne />
+    <Layout pageTitle="Home Two">
+      <BannerTwo />
+      <ThreeIconBox />
+      <PopularToursTwo />
+      <SingaporeTour />
+      <VideoTwo />
+      <CounterOne />
+      <DestinationsTwo />
+      <AboutTwo />
+      <TourTypes />
+      <BrandTwo />
+      <NewsTwo />
+      <BookNow />
     </Layout>
   );
 };
